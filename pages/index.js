625b9a27a1165686524ba0005e6ec8c891afa71e
GraziellePinheiro <66564318+GraziellePinheiro@users.noreply.github.com>
2023-10-02 18:43:13 +0000

    adiciona scripts `lint:check`e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
-function Home(){
-    return <h1>Hello World!</h1>
+function Home() {
+  return <h1>Hello World!</h1>;
 }
 
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("teste2");
+}
 export default Home;
