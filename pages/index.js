811a65218eb5ebea22cd16678cfca972e42f1c09
GraziellePinheiro <66564318+GraziellePinheiro@users.noreply.github.com>
2024-10-25 01:45:37 +0000

    Adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+function Home() {
+  return (
+    <h1>
+      Nosso tempo é cada vez mais propício à fuga dos sofrimentos e das
+      renúncias. Busca-se constantemente o caminho mais curto, mais prazeroso e
+      menos sacrificado. "É justo que muito custe o que muito vale!" #Santa
+      Teresa de Jesus#
+    </h1>
+  );
+}
+
+export default Home;
