5d2eb087a169f3c1cbf949698c7126ecda2ba839
GraziellePinheiro <66564318+GraziellePinheiro@users.noreply.github.com>
2024-10-30 00:17:33 +0000

    add `compose` config file with database service

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -2,3 +2,4 @@ test("GET to api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
 });
+const response = fetch("http://localhost:3000/api/v1/status");
