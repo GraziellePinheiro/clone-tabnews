8adb29689cba90d67244080d12e7c4200e43726a
GraziellePinheiro <66564318+GraziellePinheiro@users.noreply.github.com>
2024-11-09 20:35:12 +0000

    makes  database.js client.connect() more robust to error

--- infra/database.js
+++ infra/database.js
@@ -9,12 +9,21 @@ async function query(queryObject) {
     password: process.env.POSTGRES_PASSWORD,
   });
 
+  console.log("Credenciais do Postgres:", {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
+
   try {
     await client.connect();
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
     console.error(error);
+    throw error;
   } finally {
     await client.end();
   }
