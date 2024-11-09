658d4fb1ba5da4a5f3341b2195641db092e54eaf
GraziellePinheiro <66564318+GraziellePinheiro@users.noreply.github.com>
2024-11-09 17:32:29 +0000

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,36 @@
 import database from "infra/database.js";
+import { version } from "react";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1+1 as sum;");
-  console.log(result.rows);
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query(" SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionCount = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionCountValue =
+    databaseMaxConnectionCount.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedconnections = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  const databaseOpenedconnectionsValue =
+    databaseOpenedconnections.rows[0].count;
+
   response.status(200).json({
-    chave:
-      "'É justo que muito custe o que muito vale!' #Santa Teresa de Jesus#",
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionCountValue),
+        opened_connections: databaseOpenedconnectionsValue,
+      },
+    },
   });
 }
 
