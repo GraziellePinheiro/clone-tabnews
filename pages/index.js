97919de4ee4f747233c332ba58a412930cf3e742
GraziellePinheiro <66564318+GraziellePinheiro@users.noreply.github.com>
2026-01-27 18:24:30 +0000

    Atualiza a mensagem de reflexão!

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
 
 function Home() {
-    return <h1>Trust in God!</h1>
+    return <h1>
+        A vida é um equilíbrio entre decisões e seus reflexos, onde cada caminho tomado molda o destino pessoal, sendo importante aprender a lidar com os desdobramentos de seus atos!
+        
+    </h1>
 }
 
 export default Home;
