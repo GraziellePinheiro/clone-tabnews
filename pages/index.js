1e3d9298e857fbd5ad2b2dbd1b3934a826cf27b7
GraziellePinheiro <66564318+GraziellePinheiro@users.noreply.github.com>
2026-01-28 02:04:01 +0000

    Adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
-
 function Home() {
-    return <h1>
-        A vida é um equilíbrio entre decisões e seus reflexos, onde cada caminho tomado molda o destino pessoal, sendo importante aprender a lidar com os desdobramentos de seus atos!
-        
+  return (
+    <h1>
+      A vida é um equilíbrio entre decisões e seus reflexos, onde cada caminho
+      tomado molda o destino pessoal, sendo importante aprender a lidar com os
+      desdobramentos de seus atos!
     </h1>
+  );
 }
 
 export default Home;
