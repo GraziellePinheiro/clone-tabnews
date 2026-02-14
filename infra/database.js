2bc9c94f979fe9ae58a938ccb7621c9c350d234e
GraziellePinheiro <66564318+GraziellePinheiro@users.noreply.github.com>
2026-02-14 14:20:26 +0000

    add `process.env.NODE_ENV`

--- infra/database.js
+++ infra/database.js
@@ -9,13 +9,6 @@ async function query(queryObject) {
     database: process.env.POSTGRES_DB,
     ssl: process.env.NODE_ENV === "development" ? false : true,
   });
-  console.log("Credenciais do Postgres:", {
-    host: process.env.POSTGRES_HOST,
-    user: process.env.POSTGRES_USER,
-    port: process.env.POSTGRES_PORT,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DB,
-  });
 
   try {
     await client.connect();
