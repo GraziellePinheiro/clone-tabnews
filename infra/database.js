681a450bc9bdffed752edd82409683a7f1267dd3
GraziellePinheiro <66564318+GraziellePinheiro@users.noreply.github.com>
2026-02-14 01:44:37 +0000

    handles database ssl connection

--- infra/database.js
+++ infra/database.js
@@ -7,6 +7,7 @@ async function query(queryObject) {
     port: process.env.POSTGRES_PORT,
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DB,
+    ssl: process.env.NODE_ENV === "development" ? false : true,
   });
   console.log("Credenciais do Postgres:", {
     host: process.env.POSTGRES_HOST,
@@ -24,7 +25,7 @@ async function query(queryObject) {
     console.error(error);
     throw error;
   } finally {
-    await client.end();
+    // await client.end();
   }
 }
 
