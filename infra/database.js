a97ac05a3fdc3155e16cf4ac9a993e21f68b02de
GraziellePinheiro <66564318+GraziellePinheiro@users.noreply.github.com>
2026-02-26 01:28:56 +0000

    adds `/api/v1/migratons` endpoint

--- infra/database.js
+++ infra/database.js
@@ -1,27 +1,35 @@
 import { Client } from "pg";
 
 async function query(queryObject) {
-  const client = new Client({
-    host: process.env.POSTGRES_HOST,
-    user: process.env.POSTGRES_USER,
-    port: process.env.POSTGRES_PORT,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DB,
-    ssl: process.env.NODE_ENV === "development" ? false : true,
-  });
+  let client;
 
   try {
-    await client.connect();
+    client = await getNewClient();
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
     console.error(error);
     throw error;
   } finally {
-    // await client.end();
+    await client.end();
   }
 }
 
+async function getNewClient() {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    user: process.env.POSTGRES_USER,
+    port: process.env.POSTGRES_PORT,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    ssl: process.env.NODE_ENV === "production" ? true : false,
+  });
+
+  await client.connect();
+  return client;
+}
+
 export default {
-  query: query,
+  query,
+  getNewClient,
 };
---
+++ tests/integration/api/v1/migrations/post.test.js
@@ -0,0 +1,39 @@
+import database from "infra/database.js";
+
+beforeAll(cleanDatabase);
+
+async function cleanDatabase() {
+  await database.query("drop schema public cascade; create schema public");
+}
+
+test("POST to /api/v1/migrations should return 200", async () => {
+  const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "POST",
+  });
+  expect(response1.status).toBe(201);
+
+  const response1Body = await response1.json();
+  expect(Array.isArray(response1Body)).toBe(true);
+  expect(response1Body.length).toBeGreaterThan(0);
+  console.log(response1Body);
+  const migration = response1Body[0];
+
+  expect(migration).toMatchObject({
+    path: expect.any(String),
+    name: expect.any(String),
+    timestamp: expect.any(Number),
+  });
+
+  expect(Object.keys(migration).sort()).toEqual(
+    ["path", "name", "timestamp"].sort(),
+  );
+
+  const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "POST",
+  });
+  expect(response2.status).toBe(200);
+
+  const response2Body = await response2.json();
+  expect(Array.isArray(response2Body)).toBe(true);
+  expect(response2Body.length).toBeGreaterThan(0);
+});
